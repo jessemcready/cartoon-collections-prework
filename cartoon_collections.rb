039d3fff172dd5ea9bb47947cd0edb9b93c1b3f9
Jesse McReady <jessemcready@gmail.com>
2018-06-21 10:22:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet(veggies)# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.collect do |call|
+      
+  end
 end
 
 def long_planeteer_calls(calls)# code an argument here
