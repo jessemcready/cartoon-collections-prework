6788db9b486e92f10bce4dc75aef6217a47a1aa7
Jesse McReady <jessemcready@gmail.com>
2018-06-21 10:24:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,9 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.any? 
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese(cheese)# code an argument here
